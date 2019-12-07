e9112b436cbcb96363c38b3eabc306023bccbe38
Nelya Parfenova <nelya.parfenova@gmail.com>
2019-12-07 03:14:50 +0100

    cors origin specific

--- spotonproject/settings.py
+++ spotonproject/settings.py
@@ -31,9 +31,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,9 +123,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 
-"""CORS_ORIGIN_WHITELIST = [
+CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
     "http://localhost:8090",
-]"""
+]
